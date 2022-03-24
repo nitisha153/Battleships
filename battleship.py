b30aa637401a56ae7a0b0ca85b7feedba91be463
nitisha153 <varanasivsnitisha@msitprogram.net>
2022-03-24 17:53:29 +0530

    successfully run the game!

--- battleship.py
+++ battleship.py
@@ -79,7 +79,7 @@ def mousePressed(data, event, board):
     #print(l[0],l[1])
     if board == "user":
         clickUserBoard(data,l[0],l[1])
-    elif board =="computer":
+    elif board =="comp":
         runGameTurn(data,l[0],l[1])
     return None
     
@@ -340,9 +340,9 @@ def getComputerGuess(board):
     while board[row][col] == EMPTY_CLICKED:
         row = random.randint(0,9)
         col = random.randint(0,9)
-    if board[row][col] == EMPTY_UNCLICKED:
-        return [row,col]
-    return
+    #if board[row][col] == EMPTY_UNCLICKED:
+    return [row,col]
+    
 
 
 '''
@@ -365,14 +365,14 @@ Returns: None
 '''
 def drawGameOver(data, canvas):
     if data["winner"] == "user":
-        canvas.create_text(300,50, text = "You Won!", fill ="purple", font = ("Georgia 20 bold"))
-        canvas.create_text(300,50, text = "Press Enter to restart", fill ="red", font = ("Georgia 20 bold"))
+        canvas.create_text(300,50, text = "You Won!", fill ="black", font = ("Georgia 20 bold"))
+        canvas.create_text(300,250, text = "Press Enter to restart", fill ="red", font = ("Georgia 20 bold"))
     elif data["winner"] == "comp":
-        canvas.create_text(300,50, text = "You Lost!", fill ="red", font = ("Georgia 20 bold"))
-        canvas.create_text(300,50, text = "Press Enter to restart", fill ="red", font = ("Georgia 20 bold"))
+        canvas.create_text(300,50, text = "You Lost!", fill ="black", font = ("Georgia 20 bold"))
+        canvas.create_text(300,250, text = "Press Enter to restart", fill ="red", font = ("Georgia 20 bold"))
     elif data["winner"] == "draw":
-        canvas.create_text(300,50, text = "It's a Draw! Out of moves.", fill ="red", font = ("Georgia 20 bold"))
-        canvas.create_text(300,50, text = "Press Enter to restart", fill ="red", font = ("Georgia 20 bold"))
+        canvas.create_text(300,50, text = "It's a Draw! Out of moves.", fill ="black", font = ("Georgia 20 bold"))
+        canvas.create_text(300,250, text = "Press Enter to restart", fill ="red", font = ("Georgia 20 bold"))
     return None
 
 
